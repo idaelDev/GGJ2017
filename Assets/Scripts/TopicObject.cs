a1b245c40df95743581e1928bbcc02120f184176
Idael <judicael.abecassis@gmail.com>
2017-01-21 01:07:23 +0100

    Features, dialog database

---
+++ Assets/Scripts/TopicObject.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TopicObject{
+
+    public int TopicID { get; private set; }
+    public List<DialogObject> Dialogs { get; private set; }
+
+    public string PositiveAnswer { get; private set; }
+    public string NeutralAnswer { get; private set; }
+    public string NegativeAnswer { get; private set; }
+    public string LeaveAnswer { get; private set; }
+
+    public TopicObject(int id)
+    {
+        TopicID = id;
+    }
+
+    public void AddDialog(DialogObject obj)
+    {
+        Dialogs.Add(obj);
+    }
+    
+    public void addAnswers(string pos, string neg, string neutral, string go)
+    {
+        PositiveAnswer = pos;
+        NegativeAnswer = neg;
+        NeutralAnswer = neutral;
+        LeaveAnswer = go;
+    }
+}
